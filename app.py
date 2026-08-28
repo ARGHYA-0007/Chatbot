c23a342be0c32bf8d51cb98e7ab75d1122b7b6ef
Arghyadeep Halder <arghyadeeph95@gmail.com>
2026-08-29 00:21:36 +0530

    Upload deploy1 folder

---
+++ app.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from langchain_ollama import ChatOllama
+from fastapi.responses import JSONResponse
+from model.intro_model import intro_workflow,llm
+from model.email_model import email_workflow,llm7b
+
+app = FastAPI()
+MODEL_VERSION = '1.0.0'
+
+@app.get('/')
+def home():
+    return {'message':'this is intro model'}
+@app.get('/health')
+def health_check():
+    return {
+        'status':'ok',
+        'model version': MODEL_VERSION,
+        'model loaded':llm is not None
+    }
+@app.post('/predicting')
+def predict(input_x:str):
+    config = {"configurable": {"thread_id": "1"}}
+    try:
+        result = intro_workflow.invoke({ "intro":input_x},config = config)
+        return result
+    except Exception as e:
+        return JSONResponse(status_code=500,content=str(e))
