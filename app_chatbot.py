c4953e23817d34d71d85dc6951946c5860ffdebd
Arghyadeep Halder <arghyadeeph95@gmail.com>
2026-09-05 00:21:59 +0530

    add history using MemorySaver

--- app_chatbot.py
+++ app_chatbot.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from chatbot_model import workflow
+from langchain_core.messages import SystemMessage,HumanMessage
+
 
 
 app = FastAPI()
@@ -10,5 +12,9 @@ def hello():
     return {'message':'you are now in our chatbot named'}
 @app.post('/chatbot')
 def chatbot(query:str):
-    result = workflow.invoke({'query':query})
-    return {'AI':result['answer']}
+    result = workflow.invoke({
+    "messages": [
+        HumanMessage(content=query)
+    ]
+})
+    return {'AI':result['messages'][-1].content}
--- chatbot_model.py
+++ chatbot_model.py
@@ -1,6 +1,6 @@
 import csv
 import os
-from typing import Literal, Optional, TypedDict
+from typing import Literal, Optional, TypedDict,Annotated
 from langchain_core.messages import SystemMessage,HumanMessage
 from pydantic import BaseModel, Field
 from langgraph.graph import StateGraph, END,START
@@ -9,24 +9,28 @@ from typing import Optional
 from langchain_ollama import ChatOllama
 from langchain_groq import ChatGroq
 from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph.message import add_messages, BaseMessage
 
 llm = ChatOllama(model='qwen2.5:1.5b')
+memory = MemorySaver()
+config = {
+    "configurable": {
+        "thread_id": "user1"
+    }
+}
 class ChatState(TypedDict):
-    query:str
-    answer:str
-def ask_query(state:ChatState):
-    query = input()
-    return {'query':query}
-def chat(state:ChatState):
-    query = state['query']
-    result = llm.invoke(query).content
-    return {'answer':result}
+    messages:Annotated[list[BaseMessage], add_messages]
+def chat(state: ChatState):
+    result = llm.invoke(state["messages"])
+
+    return {"messages": [result]}
 graph = StateGraph(ChatState)
 graph.add_node('chat',chat)
 graph.add_edge(START,'chat')
 graph.add_edge('chat',END)
-workflow = graph.compile()
-# while True:
-#     query = input('USER:')
-#     result = workflow.invoke({'query':query})
-#     print('AI:',result['answer'])
+workflow = graph.compile(checkpointer=memory)
+while True:
+    query = input('USER:')
+    result = workflow.invoke({
+    "messages": [HumanMessage(content=query)]},config=config)
+    print('AI',result['messages'][-1].content)
